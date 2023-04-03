5da82a193491ff32c5cb8840d6dcfd594c76c632
aere777 <74952990+aere777@users.noreply.github.com>
2023-04-03 17:05:30 +0500

    Update solution.py

--- solution.py
+++ solution.py
@@ -4,7 +4,7 @@ import numpy as np
 from scipy.stats import norm
 
 
-chat_id = 123456 # Ваш chat ID, не меняйте название переменной
+chat_id = 721467480 # Ваш chat ID, не меняйте название переменной
 
 def solution(p: float, x: np.array) -> tuple:
     s_2=0
