50bee8378ad7c22223f5c09ad94a020d657a5721
aere777 <74952990+aere777@users.noreply.github.com>
2023-04-04 16:39:27 +0500

    Update solution.py

--- solution.py
+++ solution.py
@@ -7,6 +7,9 @@ from scipy.stats import norm
 chat_id = 721467480 # Ваш chat ID, не меняйте название переменной
 
 def solution(p: float, x: np.array) -> tuple:
+    sumOfPow=0
     alpha = 1 - p
-    return x.mean() - np.sqrt(np.var(x)) * norm.ppf(1-alpha/2) / np.sqrt(len(x)), \
-           x.mean() - np.sqrt(np.var(x))* norm.ppf(alpha/2) / np.sqrt(len(x))
+    for ele in x:
+        sumOfPow=sumOfPow+ele**2
+    return np.sqrt(sumOfPow / chi2(2 * len(x)).ppf(q=1 - alpha / 2) / 24), \
+           np.sqrt(sumOfPow / chi2(2 * len(x)).ppf(q=alpha / 2) / 24)
